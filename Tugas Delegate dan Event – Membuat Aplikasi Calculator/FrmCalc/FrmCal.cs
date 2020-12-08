7ee39bf980aef7eeacca09c0f7b5defb6c969c4d
muhamad asbi <62243901+muhamadasbi@users.noreply.github.com>
2020-12-08 15:49:01 +0700

    Add files via upload

---
+++ Tugas Delegate dan Event – Membuat Aplikasi Calculator/FrmCalc/FrmCal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmCalc
+{
+    public partial class FrmCal : Form
+    {
+        private IList<Calculator> listOfCalculator = new List<Calculator>();
+        public FrmCal()
+        {
+            InitializeComponent();
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void FrmEntry_OnCreate(Calculator cal)
+        {
+            listOfCalculator.Add(cal);
+            listBox1.Items.Add(cal.Hasil);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmEntryCal frmEntry = new FrmEntryCal("Calculator");
+            frmEntry.OnCreate += FrmEntry_OnCreate;
+            frmEntry.ShowDialog();
+        }
+
+        private void FrmCal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
